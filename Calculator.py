8cf5904c06a33cf7ef2488f3b0603c8ee9aea3ee
alok seth <alok.seth4@gmail.com>
2025-11-30 17:55:26 +0530

    Initial commit

---
+++ Calculator.py
@@ -0,0 +1,11 @@
+print ("This is basic calulator program which will take \n two number and provide summation , subtraction ,product and dividend  ")
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+summation = num1 + num2
+subtraction = num1 - num2
+product = num1 * num2
+dividend = num1 / num2
+print("The sum is: ", summation)
+print("The subtraction is: ", subtraction)
+print("The product is: ", product)
+print("The dividend is: ", dividend)
---
+++ Greeting.py
@@ -0,0 +1,6 @@
+"""This program is coded to greet to provided name"""
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+full_name = first_name + " " + last_name
+print("Welcome Mr. "+full_name)
